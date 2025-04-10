0d4aebb58af2df1b5a15a103b49497381dc20e4e
Andrii.Rohovenko <andrii.rohovenko@gmail.com>
2025-04-10 22:58:14 +0300

    splited shared implementation to shared components js/html

--- src/js/services/favorites.js
+++ src/js/services/favorites.js
@@ -1,25 +1,10 @@
-import { handleGetQuoteOfTheDay } from './quotes.js';
-import { handleGetExerciseById } from './exercises.js';
+import { renderQuoteOfTheDay } from '../sharedComponents/quoteOfTheDay.js';
+import { preparingCardsMarkup } from '../sharedComponents/exercisesCards.js';
+import { handleGetExerciseById } from '../services/exercises.js';
 
 const quoteText = document.querySelector('.quote-day-card-text');
 const quoteAuthor = document.querySelector('.quote-day-card-author');
 const workuotList = document.querySelector('.workout-list');
-
-async function renderQuoteOfTheDay() {
-  try {
-    // add logic to retrive data by key "quoteOfTheDay" from LocalStorage
-    // check if date is present if yes render if not than proceed with a request
-    const data = await handleGetQuoteOfTheDay();
-
-    const authorName = data.author;
-    const authorQuote = data.quote;
-    quoteText.textContent = authorQuote;
-    quoteAuthor.textContent = authorName;
-    console.log(quoteText);
-  } catch (error) {
-    console.log('🚀 ~ error in getQuoteOfTheDay data rendering:', error);
-  }
-}
 
 async function renderFavoritesItems() {
   try {
@@ -32,60 +17,24 @@ async function renderFavoritesItems() {
       favoritesItems.push(data);
     }
 
-    preparingFavoritesMarkup(favoritesItems);
+    preparingCardsMarkup(workuotList, favoritesItems);
   } catch (error) {
     console.log('🚀 ~ error in getFavoritesItems data rendering:', error);
   }
 }
 
-function preparingFavoritesMarkup(items) {
-  if (!items || items.length === 0) {
-    workuotList.innerHTML = '';
-    return;
-  }
-
-  const FavoritesMarkup = items
-    .map(
-      item => `
-    <li class="workout-list-item">
-      <div class="workout-card">
-        <div class="workout-header">
-          <span class="workout-badge">WORKOUT</span>
-          <button class="delete-button" aria-label="Delete workout">
-            <img src="../img/trash-icon.svg" alt="Delete" width="16" height="16">
-          </button>
-          <button class="start-button">Start ➔</button>
-        </div>
-        <div class="workout-body">
-        <span class="workout-icon-running">
-          <img
-            src="../img/quote_icon_1.svg"
-            width="24px"
-            height="24px"
-            alt="Running Icon"
-          />
-      </span>        
-          <h3 class="workout-name">${item.name}</h3>
-          <p class="workout-stats">
-            Burned calories: ${item.burnedCalories} / ${item.time} min
-            <br>
-            Body part: ${item.bodyPart} <br>  Target: ${item.target}
-          </p>
-        </div>
-      </div>
-    </li>
-    `
-    )
-    .join('');
-
-  workuotList.innerHTML = FavoritesMarkup;
-}
-
-renderQuoteOfTheDay();
+renderQuoteOfTheDay(quoteText, quoteAuthor);
 
 const mocked_data_id_1 = '64f389465ae26083f39b17a2';
 const mocked_data_id_2 = '64f389465ae26083f39b17a7';
-const mocked_items_list = [mocked_data_id_1, mocked_data_id_2];
+const mocked_data_id_3 = '64f389465ae26083f39b17a4';
+const mocked_data_id_4 = '64f389465ae26083f39b17a5';
+const mocked_items_list = [
+  mocked_data_id_1,
+  mocked_data_id_2,
+  mocked_data_id_3,
+  mocked_data_id_4,
+];
 const mocked_items_json = JSON.stringify(mocked_items_list);
 localStorage.setItem('favorites', mocked_items_json);
 
