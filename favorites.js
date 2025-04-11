d458dc2958335e62ee254a07a81e02a005c14887
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2025-04-11 18:00:21 +0000

    Deploying to gh-pages from @ AndyGrigs/your-energy@036dcd7296c5f5e154778d2e38002050a83b46ee 🚀

--- favorites.js
+++ favorites.js
@@ -1,9 +1,9 @@
-import{g as i}from"./assets/error-messages-Bd4pWVpB.js";import{a as l}from"./assets/vendor-M0pO2yrp.js";const d=async()=>{const{data:t}=await l.get("/quote");return t},u=()=>new Date().toISOString().split("T")[0],g=async()=>{try{const t=await d(),e=u(),o={...t,date:e};return localStorage.setItem("quoteOfTheDay",JSON.stringify(o)),o}catch(t){const{status:e}=t,o=i[e]||`Unexpected error (${t.message||"unknown"})`;throw{code:e,message:o}}};async function h(t,e){try{const o=JSON.parse(localStorage.getItem("quoteOfTheDay")),a=u(),r=o.date;let s,n;if(a===r)s=o.author,n=o.quote;else{const c=await g();s=c.author,n=c.quote}t.textContent=n,e.textContent=s}catch(o){console.log("🚀 ~ error in getQuoteOfTheDay data rendering:",o)}}function y(t,e,o=!0){if(!e||e.length===0){t.innerHTML="";return}const a=e.map(r=>`
+import{g as i}from"./assets/error-messages-Bd4pWVpB.js";import{a as l}from"./assets/vendor-M0pO2yrp.js";const d=async()=>{const{data:t}=await l.get("/quote");return t},u=()=>new Date().toISOString().split("T")[0],g=async()=>{try{const t=await d(),o=u(),e={...t,date:o};return localStorage.setItem("quoteOfTheDay",JSON.stringify(e)),e}catch(t){const{status:o}=t,e=i[o]||`Unexpected error (${t.message||"unknown"})`;throw{code:o,message:e}}};async function h(t,o){try{const e=JSON.parse(localStorage.getItem("quoteOfTheDay")),a=u();let r,s,n;if(e!=null)r=e.date,a===r&&(s=e.author,n=e.quote);else{const c=await g();s=c.author,n=c.quote}t.textContent=n,o.textContent=s}catch(e){console.log("🚀 ~ error in getQuoteOfTheDay data rendering:",e)}}function y(t,o,e=!0){if(!o||o.length===0){t.innerHTML="";return}const a=o.map(r=>`
     <li class="workout-list-item">
       <div class="workout-card">
         <div class="workout-header">
           <span class="workout-badge">WORKOUT</span>
-          ${o?`
+          ${e?`
             <button class="delete-button" aria-label="Delete workout">
               <img src="../img/trash-icon.svg" alt="Delete" width="16" height="16">
             </button>
@@ -28,5 +28,5 @@ import{g as i}from"./assets/error-messages-Bd4pWVpB.js";import{a as l}from"./ass
         </div>
       </div>
     </li>
-    `).join("");t.innerHTML=a}const m=document.querySelector(".quote-day-card-text"),p=document.querySelector(".quote-day-card-author"),w=document.querySelector(".workout-list");async function f(){try{const t=localStorage.getItem("favorites"),e=JSON.parse(t);if(e&&e.length>0)y(w,e);else{const o=document.querySelector(".not-items-message");o.style.display="block"}}catch(t){console.log("🚀 ~ error in getFavoritesItems data rendering:",t)}}h(m,p);f();
+    `).join("");t.innerHTML=a}const m=document.querySelector(".quote-day-card-text"),p=document.querySelector(".quote-day-card-author"),f=document.querySelector(".workout-list");async function w(){try{const t=localStorage.getItem("favorites"),o=JSON.parse(t);if(o&&o.length>0)y(f,o);else{const e=document.querySelector(".not-items-message");e.style.display="block"}}catch(t){console.log("🚀 ~ error in getFavoritesItems data rendering:",t)}}h(m,p);w();
 //# sourceMappingURL=favorites.js.map
