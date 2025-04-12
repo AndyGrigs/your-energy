d579e5eab0c01edcdfc367ce01adea48297dccc7
bonny-art <svitlana.otenko@gmail.com>
2025-04-13 00:07:57 +0300

    Refactor setActiveLink function to handle current path based on environment (localhost or live)

--- src/js/utils/setActiveNavLink.js
+++ src/js/utils/setActiveNavLink.js
@@ -1,12 +1,31 @@
 export const setActiveLink = () => {
-  const links = document.querySelectorAll('.nav-link');
-  const currentPath = window.location.pathname;
+	const links = document.querySelectorAll('.nav-link');
+	const currentPath = window.location.pathname;
+	const isLocalhost =
+		window.location.hostname === 'localhost' ||
+		window.location.hostname === '127.0.0.1';
 
-  links.forEach(link => {
-    if (link.getAttribute('href') === currentPath) {
-      link.classList.add('active');
-    } else {
-      link.classList.remove('active');
-    }
-  });
+	console.log('---------------------------------------');
+
+	console.log('🚀 ~ isLocalhost:', isLocalhost);
+
+	links.forEach(link => {
+		const linkHref = isLocalhost
+			? link.getAttribute('href')
+			: `/your-energy-test${link.getAttribute('href')}`;
+
+		console.log('---');
+		console.log('🚀 ~ linkHref:', linkHref);
+		console.log('🚀 ~ currentPath:', currentPath);
+
+		if (linkHref === currentPath) {
+			console.log(`${linkHref} is active`);
+			link.classList.add('active');
+		} else {
+			link.classList.remove('active');
+			console.log(`${linkHref} is not active`);
+		}
+
+		console.log('---');
+	});
 };
