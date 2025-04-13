7902217461ec1a97d4bb07335d9d08a333e968e0
Serhii-Palamarchuk <serhiy.palamarchuk@gmail.com>
2025-04-13 20:13:34 +0300

    Fix loader display logic in renderQuoteOfTheDay function

--- src/js/sharedComponents/quoteOfTheDay.js
+++ src/js/sharedComponents/quoteOfTheDay.js
@@ -15,7 +15,7 @@ export async function renderQuoteOfTheDay() {
 	try {
 		const ls_data = JSON.parse(localStorage.getItem('quoteOfTheDay'));
 
-		await loader.show(quoteTextcontainer);
+		await loader.show(quoteTextcontainer.parentElement);
 		const todayDate = getTodayDate();
 
 		let authorName;
@@ -35,6 +35,6 @@ export async function renderQuoteOfTheDay() {
 	} catch (error) {
 		console.log('🚀 ~ error in getQuoteOfTheDay data rendering:', error);
 	} finally {
-		await loader.hide(quoteTextcontainer);
+		await loader.hide(quoteTextcontainer.parentElement);
 	}
 }
